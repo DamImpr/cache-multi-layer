b57ba2687b73a03d5163869ef866729511f20068
Damiano Improta <damiano.improta@gmail.com>
2025-10-03 18:04:59 +0200

    modifiche docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,15 @@ elif [ "$1" = 'rector' ]; then
     exec php vendor/bin/rector process
 elif [ "$1" = 'phpstan' ]; then
     exec php vendor/bin/phpstan analyse -c phpstan.neon
+elif [ "$1" = 'sh' ]; then
+    exec sh
 fi
 
-sh
+
+echo "comands allowed"
+echo " - test-sw"
+echo " - update-vendor"
+echo " - php-cs-fix"
+echo " - rector"
+echo " - phpstan"
+echo " - sh"
